73ad9339a768bc1288d6d1fcd4d8ee798a2c0873
Mikhail Davidovich <Mikhail_Davidovich@swissre.com>
2025-04-28 14:23:20 +0400

    fixed instruction

--- init-db.sh
+++ init-db.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Проверка переменных окружения
 if [ -z "$SA_PASSWORD" ]; then
   echo "ERROR: SA_PASSWORD is not set!"
   exit 1
@@ -11,33 +10,17 @@ if [ -z "$DB_NAME" ]; then
   exit 1
 fi
 
-# Запуск MSSQL Server в фоне
 /opt/mssql/bin/sqlservr &
 
-# Получаем PID процесса
 MSSQL_PID=$!
 
 echo "Waiting for SQL Server to start..."
 
-# Цикл ожидания доступности SQL Server
-#RETRIES=30
-#until /opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -Q "SELECT 1" > /dev/null 2>&1
-#do
-#    sleep 5
-#    RETRIES=$((RETRIES-1))
-#    echo -n "."
-#    if [ $RETRIES -le 0 ]; then
-#      echo "SQL Server did not start in time."
-#      kill $MSSQL_PID
-#      exit 1
-#    fi
-#done
 
 echo ""
 sleep 40
 echo "SQL Server is up!"
 
-# Проверка и создание базы данных
 echo "Checking and creating database $DB_NAME..."
 /opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -C -Q "IF DB_ID(N'$DB_NAME') IS NULL CREATE DATABASE [$DB_NAME];"
 
@@ -47,5 +30,4 @@ else
   echo "Failed to execute database creation command."
 fi
 
-# Ожидание завершения процесса MSSQL
 wait $MSSQL_PID
