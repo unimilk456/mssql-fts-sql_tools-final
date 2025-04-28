ab35c4e2ede8146d37fc7f88da99f9ccd4a61f5b
Mikhail Davidovich <Mikhail_Davidovich@swissre.com>
2025-04-28 11:01:10 +0400

    added a few files

--- init-db.sh
+++ init-db.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Проверка переменных окружения
 if [ -z "$SA_PASSWORD" ]; then
   echo "ERROR: SA_PASSWORD is not set!"
+  exit 1
+fi
+
+if [ -z "$DB_NAME" ]; then
+  echo "ERROR: DB_NAME is not set!"
   exit 1
 fi
 
@@ -12,11 +18,34 @@ fi
 MSSQL_PID=$!
 
 echo "Waiting for SQL Server to start..."
-sleep 20
 
+# Цикл ожидания доступности SQL Server
+#RETRIES=30
+#until /opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -Q "SELECT 1" > /dev/null 2>&1
+#do
+#    sleep 5
+#    RETRIES=$((RETRIES-1))
+#    echo -n "."
+#    if [ $RETRIES -le 0 ]; then
+#      echo "SQL Server did not start in time."
+#      kill $MSSQL_PID
+#      exit 1
+#    fi
+#done
+
+echo ""
+sleep 40
+echo "SQL Server is up!"
+
+# Проверка и создание базы данных
 echo "Checking and creating database $DB_NAME..."
-/opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -C -Q "IF DB_ID(N'$DB_NAME') IS NULL CREATE DATABASE [$DB_NAME];" \
-&& echo " Database check completed." \
-|| echo "Failed to execute database creation command."
-# Ожидание завершения MSSQL процесса (он будет работать в foreground)
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -C -Q "IF DB_ID(N'$DB_NAME') IS NULL CREATE DATABASE [$DB_NAME];"
+
+if [ $? -eq 0 ]; then
+  echo "Database check completed successfully."
+else
+  echo "Failed to execute database creation command."
+fi
+
+# Ожидание завершения процесса MSSQL
 wait $MSSQL_PID
